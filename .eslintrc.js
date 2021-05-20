8dc8ffeb6de2b5f10e63d89e54601bc0938f3a22
John Minns <john.minns@missguided.com>
2021-05-20 21:35:19 +0100

    Correct issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: "module",
+    sourceType: 'module',
     project: [
       './tsconfig.eslint.json'
     ]
   },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended"
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
   rules: {},
-  ignorePatterns: [
-    '/*.js',
-    '/dist/**/*',
-    '/node_modules/**/*'
-  ],
+  ignorePatterns: ['/*.js', '/dist/**/*', '/node_modules/**/*'],
 };
